e56d34277b7edc99456182ab47028bc7d32e86d9
KaKaCodes1 <139960971+KaKaCodes1@users.noreply.github.com>
2024-03-14 20:27:45 +0300

    Update datatypes.dart
    Adding additional information

--- datatypes.dart
+++ datatypes.dart
@@ -2,21 +2,21 @@
 an understanding of data types in Dart
 */
 void main(){
-  //integer data type
+  //integer data type. The variable holds my age
   int age=19;
-  //double data type
+  //double data type. The variable holds the money saved
   double moneySaved=13000.50;
-  //String data type
+  //String data type. The variable holds my name
   String name="Hamisi Juma";
-  //List data type
-  List<String>mySongs=["Prove it","Made for me","Saturn","Selfish"];
-  //Map data type
+  //List data type. The variable holds my movies
+  List<String>myMovies=["Deadpool","DaVinciCode","Infinite","Shrek"];
+  //Map data type. The variable holds my units and their code
   Map<int,String>units={100:"Web Development",200:"Dart"};
   
   //print the data
   print('Name: $name');
   print('Age: $age');
   print('Money Saved: $moneySaved');
-  print('My Songs: $mySongs');
+  print('My Movies: $myMovies');
   print('Units: $units');
 }
