7b115b7fae3d84858b28b1aaecfed11f6040a9a3
KaKaCodes1 <hamisig123@gmail.com>
2024-03-14 20:02:28 +0300

    My first dart committ

---
+++ datatypes.dart
@@ -0,0 +1,22 @@
+/*Dart program to demonstrate 
+an understanding of data types in Dart
+*/
+void main(){
+  //integer data type
+  int age=19;
+  //double data type
+  double moneySaved=13000.50;
+  //String data type
+  String name="Hamisi Juma";
+  //List data type
+  List<String>mySongs=["Prove it","Made for me","Saturn","Selfish"];
+  //Map data type
+  Map<int,String>units={100:"Web Development",200:"Dart"};
+  
+  //print the data
+  print('Name: $name');
+  print('Age: $age');
+  print('Money Saved: $moneySaved');
+  print('My Songs: $mySongs');
+  print('Units: $units');
+}
